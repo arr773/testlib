8b056c7f5ff6226a605c6493e2c200c98c658bc7
Aaryan Agarwal <aaryanatharva@gmail.com>
2023-07-20 21:27:12 +0530

    published lib

--- medium/main.py
+++
@@ -1,9 +0,0 @@
-
-# Import Multiplication from your library
-from medium_multi.multi import Multiplication
-
-# Instantiate a Multiplication object
-multiplication = Multiplication(2)
-
-# Call the multiply method
-print(multiplication.multiply(5))
