c8f192092be9d8cdf5e4c1d18a48b7b7e1bff0bc
Aaryan Agarwal <aaryanatharva@gmail.com>
2023-07-21 10:33:30 +0530

    Update main.py

--- mediumvers123/main.py
+++ mediumvers123/main.py
@@ -7,3 +7,11 @@ multiplication = Multiplication(2)
 
 # Call the multiply method
 print(multiplication.multiply(5))
+
+#Steps to publish the library
+#create the dist file
+#python setup.py sdist bdist_wheel
+#python -m twine check dist/*
+#python -m twine upload dist/*
+# username=TestLib
+# password=test_lib
