3c0122276f765ca6ff708508fb446eef48442657
Aaryan Agarwal <aaryanatharva@gmail.com>
2023-08-09 14:00:22 -0400

    update module

--- mediumvers123/main.py
+++ mediumvers123/main.py
@@ -1,17 +1,27 @@
 
-# Import Multiplication from your library
-from medium_multi.multi import Multiplication
+# # Import Multiplication from your library
+# from medium_multi.multi import Multiplication
 
-# Instantiate a Multiplication object
-multiplication = Multiplication(2)
+# # Instantiate a Multiplication object
+# multiplication = Multiplication(2)
 
-# Call the multiply method
-print(multiplication.multiply(5))
+# # Call the multiply method
+# print(multiplication.multiply(5))
 
-#Steps to publish the library
-#create the dist file
-#python setup.py sdist bdist_wheel
-#python -m twine check dist/*
-#python -m twine upload dist/*
-# username=TestLib
-# password=test_lib
+# #Steps to publish the library
+# #create the dist file
+# #python setup.py sdist bdist_wheel
+# #python -m twine check dist/*
+# #python -m twine upload dist/*
+# # username=TestLib
+# # password=test_lib
+import pandas as pd
+from medium_multi.stats import reportgeneration
+real_data = pd.read_csv("titanic_train.csv")
+synthetic_data = pd.read_csv('gretel_titanic_data.csv')
+output="test.pdf"
+report=reportgeneration(real_data,synthetic_data)
+user_name = input("Enter User Name: ")
+tool_used = input("Enter the Tool Used for Synthetic Data Generation: ")
+# doc = SimpleDocTemplate(output, pagesize=letter)
+report.create_pdf_report(output,user_name,tool_used)
--- mediumvers123/setup.py
+++ mediumvers123/setup.py
@@ -16,7 +16,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="mediumvers123",
-    version="0.1.1",
+    version="0.1.2",
     description="Demo library",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -37,5 +37,5 @@ setup(
     ],
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["numpy","pandas","scipy","skleran","reportlab","matplotlib","io"]
+    install_requires=["numpy","pandas","scipy","sklearn","reportlab","matplotlib","io"]
 )
